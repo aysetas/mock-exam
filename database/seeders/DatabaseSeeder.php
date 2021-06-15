7c2f4da084c6c620f04d88640d5ad428fa7c8c90
Ayşe TAŞ <41993475+aysetas@users.noreply.github.com>
2021-06-15 13:00:22 +0300

    isAdmin middleware oluşturuldu. :tada:

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::insert([
+        'name' => 'Ayşe Taş',
+        'email' => 'aysetas@gmail.com',
+        'type' => 'admin',
+        'email_verified_at' => now(),
+        'password' => bcrypt(123456),
+        'remember_token' => Str::random(10),
+    ]);
+         User::factory(5)->create();
     }
 }
